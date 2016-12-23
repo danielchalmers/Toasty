230bbb55a0d0d988284727759677b1006c2e84da
Daniel Chalmers <MoonDPC@gmail.com>
2016-12-23 00:46:06 -0500

    Handle unhandled exceptions

--- Notiify/App.xaml.cs
+++ Notiify/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 using Hardcodet.Wpf.TaskbarNotification;
+using Notiify.Classes;
 
 namespace Notiify
 {
@@ -8,12 +10,24 @@ namespace Notiify
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
         public static TaskbarIcon TrayIcon { get; private set; }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
             TrayIcon = (TaskbarIcon) Current.FindResource("TrayIcon");
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Popup.Show($"An unhandled exception occurred:\n\n{e.Exception}",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
         }
     }
 }
---
+++ Notiify/Classes/Popup.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Notiify.Properties;
+
+namespace Notiify.Classes
+{
+    public static class Popup
+    {
+        public static MessageBoxResult Show(string text, MessageBoxButton button = MessageBoxButton.OK,
+            MessageBoxImage image = MessageBoxImage.Information, MessageBoxResult defaultButton = MessageBoxResult.OK)
+        {
+            try
+            {
+                return MessageBox.Show(text, Resources.AppName, button, image, defaultButton);
+            }
+            catch
+            {
+                return MessageBoxResult.Cancel;
+            }
+        }
+    }
+}
